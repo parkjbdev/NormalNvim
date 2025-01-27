dc0ee487e2d294faf7842a5be8baab261efbf3fb
parkjbdev <dev@parkjb.com>
2025-01-28 04:30:44 +0900

    Update Configs - replace vim-surround to nvim-surround

--- lua/plugins/1-base-behaviors.lua
+++ lua/plugins/1-base-behaviors.lua
@@ -713,8 +713,16 @@ return {
         channel = "stable" -- stable/nightly
     }
   },
-
-  { "tpope/vim-surround" },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
   {
     "Isrothy/neominimap.nvim",
     init = function()
